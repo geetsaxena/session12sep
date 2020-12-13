ff93337b688d3ced88cc10970eacc1ade8c0b9ee
Ashish <ashish.saxena25@gmail.com>
2020-12-12 23:16:48 -0500

    SwitchCase

---
+++ src/Telcom.java
@@ -0,0 +1,18 @@
+import com.sun.xml.internal.ws.api.streaming.XMLStreamReaderFactory;
+
+public class Telcom {
+    public static void main(String[] args) {
+        int countryCode = 5;
+        switch (countryCode) {
+            case 1: {
+                System.out.println(" Dummy 1");break; }
+
+            case 2: {
+                System.out.println("Dummy 2");break; }
+            case 3: {
+                System.out.println(" Dummy 3");break; }
+            case 4: {
+                System.out.println(" Dummy 4");break; }
+            default: {
+                System.out.println(" no match. please try again");
+            } } }}
